0acf750cefe4e1f04e2e369667ef2a645bc663c0
Dennis Kennetz <drkennetz@gmail.com>
2024-03-03 13:07:51 -0600

    FIX: removed unintentional line.

--- pythonConcurrency/pool_map.py
+++ pythonConcurrency/pool_map.py
@@ -6,6 +6,4 @@ def square(x):
 if __name__ == "__main__":
     with mp.Pool(processes=mp.cpu_count()-1) as pool:
         result = pool.map(square, [1, 2, 3, 4, 5])
-        result2 = pool.map_async(square, [1, 2, 3, 4, 5])
     print(result)
-    print(result2)
